929a5882891dd0ffb685481758cc72fa0dddc5f8
Jakob Anderson <janderson@familysearch.org>
2017-04-02 17:46:55 -0600

    adding in new problem module

--- problems/arrays.js
+++ problems/arrays.js
@@ -8,9 +8,30 @@ module.exports = {
       answer: `const fruits = ['Apple', 'Banana'];`,
       correctOutput: `['Apple', 'Banana']`,
       tests: [
-        `assert.deepEqual(output, correctOutput) === undefined`
-        `importants[0].type === 'Identifier'`,
-
+        {
+          name: 'Output must be correct',
+          test: `assert.deepEqual(output, correctOutput) === undefined`
+        },
+        {
+          name: 'Must use var, let, or const',
+          test: `importants[0].type === 'Identifier'`,
+        },
+        {
+          name: 'Must use an Array',
+          test: `parsed.body[0].declarations[0].init.type === 'ArrayExpression'`,
+        },
+        {
+          name: '',
+          test: `parsed.body[0].declarations[0].init.elements.length === 2`,
+        },
+        {
+          name: '',
+          test: `parsed.body[0].declarations[0].init.elements[0].value === 'Apple'`,
+        },
+        {
+          name: '',
+          test: `parsed.body[0].declarations[0].init.elements[1].value === 'Banana'`
+        }
       ]
     }
 
