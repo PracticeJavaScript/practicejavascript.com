3996e331d1f6787376e039e8ea1c5b1a6a5b7316
Jakob Anderson <janderson@familysearch.org>
2017-04-09 20:42:40 -0600

    messing with some build opto

--- problems/arrays.js
+++ problems/arrays.js
@@ -15,6 +15,51 @@ module.exports = [
         }
       }
     ]
+  },
+  {
+    name: 'Access Array item by index (first)',
+    prompt: 'Return the first value of the Array',
+    given: `const fruits = ['apple', 'banana'];\r`,
+    answer: `const fruits = ['apple', 'banana'];\rreturn fruits[0];`,
+    tests: [
+      {
+        name: 'Output must be correct',
+        type: 'assertCorrectOutput',
+        test: function(output) {
+          return (assert.deepEqual(output, 'apple') === undefined);
+        }
+      }
+    ]
+  },
+  {
+    name: 'Access Array item by index (second)',
+    prompt: 'Return the second value of the Array',
+    given: `const fruits = ['apple', 'banana'];\r`,
+    answer: `const fruits = ['apple', 'banana'];\rreturn fruits[1];`,
+    tests: [
+      {
+        name: 'Output must be correct',
+        type: 'assertCorrectOutput',
+        test: function(output) {
+          return (assert.deepEqual(output, 'banana') === undefined);
+        }
+      }
+    ]
+  },
+  {
+    name: 'Loop over array',
+    prompt: 'Loop over the array, and push each fruit into a new array, then return the new array',
+    given: `const fruits = ['apple', 'banana'];\r`,
+    answer: `const fruits = ['apple', 'banana'];\rconst newFruits = [];\rfruits.forEach(function(item) {\r  newFruits.push(item);\r});\rreturn newFruits;`,
+    tests: [
+      {
+        name: 'Output must be correct',
+        type: 'assertCorrectOutput',
+        test: function(output) {
+          return (assert.deepEqual(output, ['apple', 'banana']) === undefined);
+        }
+      }
+    ]
   }
 
 ];
